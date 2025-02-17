e88d6371ea0e7b5c92fd81faad894406f16c1dcc
Mohammed Khan <mkha5549@stu.raritanval.edu>
2025-02-16 20:22:34 -0500

    editing  some files and adding code to the calculator.js

--- calculator.js
+++ calculator.js
@@ -14,8 +14,34 @@ const num2 = parseFloat(process.argv[4]);
 
 let result = `Solving ${num1} ${operation} ${num2}`;
 
-// Implementation goes here
-// if you get an error, exit with an error of 1 by calling process.exit(1);
+if (typeof num1 !== "number" || typeof num2 !== "number") {
+  console.log("invalid number");
+  process.exit(1);
+}
+
+if (operation === "add") {
+  result = add(num1, num2);
+} else if (operation === "subtract") {
+  result = subtract(num1, num2);
+} else if (operation === "multiply" || operation === "times") {
+  result = times(num1, num2);
+} else if (operation === "divide") {
+  result = divide(num1, num2);
+} else if (operation === "modulus" || operation === "remainder") {
+  result = modulus(num1, num2)
+} else if (operation === "power") {
+  result = power(num1, num2);
+} else {
+  console.error("unknown operation");
+  process.exit(1);
+}
+
+
+if (isNaN(result)) {
+  console.error("invalid number")
+  process.exit(1);
+}
+
 
 
 console.log("result: ", result);
